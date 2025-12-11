10465fa4dc533d3c8bd038e2f7ab4a316d3d8d89
howlers0612 <Howlers.dev@gmail.com>
2025-12-11 23:52:54 +0530

    3

--- handicraft/settings.py
+++ handicraft/settings.py
@@ -221,12 +221,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+if DEBUG:    
+   MEDIA_URL = '/media/'
+   MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+ 
 
 # Cloudinary Configuration
-# Cloudinary Configuration
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
@@ -245,9 +250,6 @@ if CLOUDINARY_URL:
         })
     except Exception as e:
         print(f"Error parsing CLOUDINARY_URL: {e}")
-
-
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
 
