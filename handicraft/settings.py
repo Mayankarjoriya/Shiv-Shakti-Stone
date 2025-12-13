7e8dda68f8e4fc2c96725da0fc3b40024e21a1be
Mayank Rajoriya <mayankrajoriya2004@gmail.com>
2025-12-13 16:33:48 +0530

    Update settings for Cloudinary and static files
    Refactor Cloudinary and static file settings based on DEBUG mode.

--- handicraft/settings.py
+++ handicraft/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 import os
 import environ
 import urllib.parse as urlparse
+import Cloudinary
 
 # Manually load .env file
 dotenv_path = os.path.join(Path(__file__).resolve().parent, '.env')
@@ -48,7 +49,7 @@ DEBUG = env("DEBUG")
 
 
 # ALLOWED_HOSTS = ['127.0.0.1', 'localhost'] + env('ALLOWED_HOSTS', default="").split()
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = env('ALLOWED_HOSTS')
 
 
 # Application definition
@@ -220,18 +221,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-if DEBUG:
-    STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),]
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-else:
-    
-    DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
-    CLOUDINARY_STORAGE = {
+CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
@@ -240,9 +232,18 @@ else:
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
-# Cloudinary Configuration
+if DEBUG is False:
+    STATICFILES_DIRS = [ BASE_DIR / 'static' ]
+    STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+    
+else:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static'),
+    ]
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # CLOUDINARY_URL = os.environ.get('CLOUDINARY_URL')
 
 
